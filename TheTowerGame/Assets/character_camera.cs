b179989da6bb06032872b71e8f34384fb9c1046f
ethanwang <ethan573@gmail.com>
2018-03-12 19:07:57 +0800

    fixed camera

--- TheTowerGame/Assets/character_camera.cs
+++ TheTowerGame/Assets/character_camera.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class character_camera : MonoBehaviour {
 
 	// Use this for initialization
+	public GameObject player;
 	void Start () {
 		
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
-		transform.position = new Vector3 (GetComponentInParent<Transform>().position.x - Camera.main.ScreenToWorldPoint (Input.mousePosition).x,0,-10);
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition) - player.transform.position;
+		float updatePos = player.transform.position.y + mousePos.y/ 2.25f;
+		updatePos = Mathf.Max(8.8f,updatePos);
+		updatePos = Mathf.Min (updatePos,36f);
+		transform.position = new Vector3 (0.64f, updatePos, -10);
 
 	}
 }
