03dc2aa192002dcbec37cb71f98583522dc189fe
Edward Huang <edwardhuang@Edwards-MacBook-Pro.local>
2018-03-15 18:29:55 +0800

    one tower has health working on other ones

--- TheTowerGame/Assets/TowerHealth.cs
+++ TheTowerGame/Assets/TowerHealth.cs
@@ -7,8 +7,11 @@ public class TowerHealth : MonoBehaviour {
 
 	float currentHealth;
 	public float health = 1f;
-	public Image healthbar;
+	public Image healthBar;
 	float full = 2;
+	public float meleeDamage = 0.1f;
+	float countHit;
+	float hitRate = 35;
 
 	void Start(){
 		currentHealth = health;
@@ -16,22 +19,25 @@ public class TowerHealth : MonoBehaviour {
 
 
 	void OnCollisionEnter2D(Collision2D c){
+		if (c.gameObject.tag == "friendlyweapon") {
+			TakeDamage (c.gameObject.GetComponent<BulletBehavior>().damage);
+		}
+	}
 
-		if (this.gameObject.tag == "smallFriendlyTower" || this.gameObject.tag == "largeFriendlyTower") {
-			if (c.gameObject.tag == "enemyweapon") {
-				TakeDamage (c.gameObject.GetComponent<BulletBehavior> ().damage);
-			}
-		} 
-
-		if(this.gameObject.tag == "smallEnemyTower" || this.gameObject.tag == "largeEnemyTower"){
-			if (c.gameObject.tag == "friendlyweapon") {
-				TakeDamage (c.gameObject.GetComponent<BulletBehavior> ().damage);
+	void OnCollisionStay2D(Collision2D c){
+		if (c.gameObject.tag == "friendly") {
+			countHit++;
+			if (countHit > c.gameObject.GetComponent<EnemyDetection>().fireRate) {
+				TakeDamage (c.gameObject.GetComponent<EnemyDetection> ().meleeDamage);
+				countHit = 0;
 			}
 		}
 	}
 
+
 	public void TakeDamage(float amount) {
 		currentHealth -= amount;
+		//healthBar.fillAmount = currentHealth / 100;
 		if (currentHealth <= 0) {
 			Die ();
 		}
@@ -43,6 +49,6 @@ public class TowerHealth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		healthbar.fillAmount = currentHealth / health;
+		healthBar.fillAmount = currentHealth / health;
 	}
 }
